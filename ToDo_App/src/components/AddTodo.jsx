1a17d65953c7fc91b0159b3ac5f2f568e42e0760
Sinchana-M-S <sinchanams932@gmail.com>
2025-07-26 23:20:05 +0530

    Version 1 done

---
+++ ToDo_App/src/components/AddTodo.jsx
@@ -0,0 +1,19 @@
+function AddTodo() {
+  return (
+    <div class="container text-center">
+      <div class="row my-row">
+        <div class="col-6">
+          <input type="text" placeholder="Enter TODO here" />
+        </div>
+        <div class="col-4">
+          <input type="date" />
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-success my-btn">Add</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodo;
---
+++ ToDo_App/src/components/TodoItem1.jsx
@@ -0,0 +1,23 @@
+function TodoItem1() {
+
+let todoName = 'DSA';
+let todoDate = '31-10-2025';
+
+  return (
+    <div class="container">
+      <div class="row my-row">
+        <div class="col-6">
+          {todoName}
+        </div>
+        <div class="col-4">
+          {todoDate}
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger my-btn">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem1;
